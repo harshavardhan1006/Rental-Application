28b97401ecd923a8d4af9cd4d06f2d1a84e8415d
CH HARSHA VARDHAN REDDY <harshasai1026@gmail.com>
2026-03-15 22:30:43 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: "https://airbnb-mern-1-sh6k.onrender.com/",
   headers: { 'Content-Type': 'application/json' }
 });
 
